3d720b70ca908d7b528553546d6c83d51978d011
Ben Holgate <holgateb@hotmail.com>
2022-11-15 15:17:27 -0800

    pseudo code thought-routes.

--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -9,3 +9,13 @@ const {
     addReaction,
     removeReaction,
 } = require('../../controllers/thought-controller');
+
+// /api/thoughts
+
+// /api/thoughts/:thoughtId
+
+// /api/thoughts/:thoughtId/reactions
+
+// /api/thoughts/:thoughtId/reactionsId
+
+module.exports = router;
