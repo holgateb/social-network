6b9976cc74475f57b05b39cd112b46671c681efc
Ben Holgate <holgateb@hotmail.com>
2022-11-15 13:59:35 -0800

    finished thought model and updated index.

--- models/Thought.js
+++ models/Thought.js
@@ -1,11 +1,46 @@
 const { Schema, model } = require('mongoose');
+const Reaction = require('./Reaction');
 
+// Schema to create Thought model
 const thoughtSchema = new Schema(
-    {
-        thoughtText: {
-            type: String,
-            required: true,
-            
-        }
-    }
-)
+  {
+    thoughtText: {
+        type: String,
+        required: true,
+        minLength: 1,
+        maxLength: 280,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: formatDate,
+    },
+    username: {
+        type: String,
+        required: true,
+    },
+    reactions: [Reaction],
+  },
+
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+thoughtSchema
+  .virtual('reactionCount')
+  .get(function () {
+    return this.reactions.length;
+  });
+
+function formatDate(date) {
+    return date.toLocaleString();
+  }; 
+
+const Thought = model('Thought', thoughtSchema);
+
+module.exports = Thought;
