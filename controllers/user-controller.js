d79870374be0f99c0cdb88081eee63af614139de
Ben Holgate <holgateb@hotmail.com>
2022-11-16 13:12:30 -0800

    psuedo user controller.

---
+++ controllers/user-controller.js
@@ -0,0 +1,16 @@
+const { User, Thought } = require('../models');
+
+// Get all users
+
+// Get single user by ID
+
+// create user
+
+// Update user by ID
+
+// Delete user
+
+// Add Friend
+
+// Remove friend
+
