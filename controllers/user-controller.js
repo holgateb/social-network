0265d4b7be48850ba202a1299878b46f0b8993f9
Ben Holgate <holgateb@hotmail.com>
2022-11-16 13:15:50 -0800

    user controller fleshed out.

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -2,9 +2,35 @@ const { User, Thought } = require('../models');
 
 // Get all users
 
+module.exports = {
+    getUsers(req, res) {
+        User.find()
+          .then((users) => res.json(users))
+          .catch((err) => res.status(500).json(err));
+      },
+}
+
 // Get single user by ID
 
+getSingleUser(req, res) {
+    User.findOne({ _id: req.params.userId })
+      .select('-__v')
+      .populate('thoughts friends')
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: 'No user with that ID' })
+          : res.json(user)
+      )
+      .catch((err) => res.status(500).json(err));
+}
+
 // create user
+
+createUser(req, res) {
+    User.create(req.body)
+    .then((user) => res.json(user))
+    .catch((err) => res.status(500).json(err));
+}
 
 // Update user by ID
 
